3967d42cb5b3fc5271236a9ba0c6e4c2dc7adcd6
Andrey <andrey1om@users.noreply.github.com>
2021-01-14 20:21:38 +0300

    Back
    Нормальная версия (предыдущие 3 не запускаются)

--- GUI/GUI.m
+++ GUI/GUI.m
@@ -1,4 +1,4 @@
-unction varargout = GUI(varargin)
+function varargout = GUI(varargin)
 %GUI MATLAB code file for GUI.fig
 %      GUI, by itself, creates a new GUI or raises the existing
 %      singleton*.
@@ -22,11 +22,7 @@ unction varargout = GUI(varargin)
 
 % Edit the above text to modify the response to help GUI
 
-<<<<<<< HEAD
 % Last Modified by GUIDE v2.5 11-Jan-2021 18:25:47
-=======
-% Last Modified by GUIDE v2.5 22-Dec-2020 22:59:33
->>>>>>> parent of d28186e... Старая версия
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -615,6 +611,7 @@ else
     rotate3d off
 end
 Way = [x(1,FullRute(1,:)); y(1,FullRute(1,:)); z(1,FullRute(1,:))];
+display(Way);
 hold on
    DrawLines(Way,CheckPoint,cost,Start,Fin,FullRute,x,y,z,colormatrix);
 hold off
@@ -680,7 +677,6 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 
-<<<<<<< HEAD
 % --- Executes on button press in pushbutton11.
 function pushbutton11_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton11 (see GCBO)
@@ -690,291 +686,15 @@ handles.pushbutton11.Visible='off';
 handles.pushbutton12.Visible='on';
 handles.uipanel7.Visible='on';
 handles.uipanel8.Visible='off';
-=======
-
-
->>>>>>> parent of d28186e... Старая версия
 
 % --- Executes on button press in pushbutton12.
 function pushbutton12_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton12 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-<<<<<<< HEAD
 handles.pushbutton12.Visible='off';
 handles.pushbutton11.Visible='on';
 handles.uipanel7.Visible='off';
 handles.uipanel8.Visible='on';
 word = actxserver('Word.Application');
 wdoc = word.Documents.Open('C:\Users\Om\Desktop\Theory.docx');
-=======
-global K
-if handles.radiobutton10.Value==1%тест кластеризации
-   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%настрока видимости
-    handles.uipanel4.Visible=1;
-    handles.uipanel8.Visible=1;
-   %%%%%%%%%%%%%%%%%%%%%%%%%%  
-elseif handles.radiobutton11.Value==1%тест построения маршрутов
-    K=0;
-    pushbutton1_Callback(hObject, eventdata, handles);
-    handles.uipanel10.Visible=1;
-elseif handles.radiobutton12.Value==1
-end
-
-% --- If Enable == 'on', executes on mouse press in 5 pixel border.
-% --- Otherwise, executes on mouse press in 5 pixel border or over pushbutton12.
-function pushbutton12_ButtonDownFcn(hObject, eventdata, handles)
-% hObject    handle to pushbutton12 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-
-function edit8_Callback(hObject, eventdata, handles)
-% hObject    handle to edit8 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit8 as text
-%        str2double(get(hObject,'String')) returns contents of edit8 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit8_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit8 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function edit9_Callback(hObject, eventdata, handles)
-% hObject    handle to edit9 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit9 as text
-%        str2double(get(hObject,'String')) returns contents of edit9 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit9_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit9 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function edit10_Callback(hObject, eventdata, handles)
-% hObject    handle to edit10 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit10 as text
-%        str2double(get(hObject,'String')) returns contents of edit10 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit10_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit10 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on button press in pushbutton13.
-function pushbutton13_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton13 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-handles.uipane7.Visible=xor(handles.uipanel7.Visible,1);
-
-% --- Executes on button press in pushbutton14.
-function pushbutton14_Callback(hObject, eventdata, handles)%тест кластеризации
-% hObject    handle to pushbutton14 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-PointCount=str2double(get(handles.edit3,'String'));
-Random = str2double(get(handles.edit4, 'String'));
-downstep=str2double(get(handles.edit11,'String'));
-step=str2double(get(handles.edit9,'String'));
-start_radius=str2double(get(handles.edit8,'String'));
-fin_radius=str2double(get(handles.edit10,'String'));
-Scale=200;
-rng(Random); 
-cluster_radius=[];
-dots_count=[];
-dots_matrix=[];
-dots_table_names=[];  
-for rad=start_radius:step:fin_radius
-    cluster_radius=[cluster_radius; rad];      
-end
-radius_table=table(cluster_radius);
-tempPC=PointCount;
-while tempPC>0
-   for rad=start_radius:step:fin_radius
-        x=rand(1,tempPC)*Scale;
-        y=rand(1,tempPC)*Scale;
-        if handles.radiobutton5.Value==1 %если 2д
-            z=zeros(1,tempPC);
-        elseif handles.radiobutton6.Value==1 %3д
-            z=rand(1,tempPC)*Scale;
-        end
-       [clusterCoord clusterDots]=Forel(x,y,z,rad,Scale);
-       dots_count=[dots_count; size(clusterDots,3)];
-   end
-       dots_matrix=[dots_matrix dots_count];%кол-во точек в каждом кластере
-       dots_count=[];
-       dots_table_names=[dots_table_names,strcat("Clusters_count_(",num2str(tempPC),"_dots)")];
-       tempPC=tempPC-downstep;
-end
-dots_table=array2table(dots_matrix);
-dots_table.Properties.VariableNames=dots_table_names;
-result_table=cat(2,radius_table,dots_table); 
-name=strcat('t1_Rng',num2str(Random),'_PC',num2str(PointCount),'_Scale',num2str(Scale),'.xls');
-[filename path]=uiputfile(name);
-path=strcat(path,filename);
-writetable(result_table,path,'Sheet',filename);
-
-
-% --- Executes on button press in pushbutton15.
-function pushbutton15_Callback(hObject, eventdata, handles)%тест маршрутов
-% hObject    handle to pushbutton15 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global x y z Start Fin priority Random PointCount
-Scale=200;
-step=str2double(get(handles.edit14,'String'));
-startradius=str2double(get(handles.edit12,'String'));
-endradius=str2double(get(handles.edit15,'String'));
-cost=NaN;
-rute_array=[];
-cost_array=[];
-rad_array=[];
-for i=startradius:step:endradius
-[cost,FullRute]=main(x,y,z,priority,Start,Fin,i,0,[]);
-if cost>2
-    cost=NaN;
-    FullRute=[];
-end
-rute_array=[rute_array;length(FullRute)];
-cost_array=[cost_array;cost];
-rad_array=[rad_array;i];
-end
-result_table=table(rad_array,rute_array,cost_array);
-result_table.Properties.VariableNames={'action_radius','dots_in_rute','rute_cost'};
-name=strcat('t2_Rng',num2str(Random),'_PC',num2str(PointCount),'_Scale',num2str(Scale),'.xls');
-[filename path]=uiputfile(name);
-path=strcat(path,filename);
-writetable(result_table,path,'Sheet',filename);
-
-
-% --- Executes on selection change in popupmenu1.
-function popupmenu1_Callback(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popupmenu1
-
-
-% --- Executes during object creation, after setting all properties.
-function popupmenu1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function edit15_Callback(hObject, eventdata, handles)
-% hObject    handle to edit15 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit15 as text
-%        str2double(get(hObject,'String')) returns contents of edit15 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit15_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit15 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function edit11_Callback(hObject, eventdata, handles)
-% hObject    handle to edit11 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit11 as text
-%        str2double(get(hObject,'String')) returns contents of edit11 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit11_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit11 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-
-
-function edit16_Callback(hObject, eventdata, handles)
-% hObject    handle to edit16 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit16 as text
-%        str2double(get(hObject,'String')) returns contents of edit16 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit16_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit16 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
->>>>>>> parent of d28186e... Старая версия
